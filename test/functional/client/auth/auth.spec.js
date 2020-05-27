4a5a8903c8de6da6f7197586793a80cd1096cfcc
luanbrandao <luanbrandao4@gmail.com>
2020-05-27 13:19:36 -0300

    test(auth.spec.js): confirmation Mail
    verifica se a conta do usuário é ativada depois que ele clica no link enviado para o e-mail.

--- test/functional/client/auth/auth.spec.js
+++ test/functional/client/auth/auth.spec.js
@@ -4,10 +4,11 @@ const { test, trait, afterEach } = use('Test/Suite')('User')
 // permite usar as requisições para API
 trait('Test/ApiClient')
 const Mail = use('Mail')
-
 const User = use('App/Models/User')
+const ConfirmationMail = use('App/Models/ConfirmationMail')
 
 afterEach(async () => {
+  await ConfirmationMail.query().delete()
   await User.query().delete()
 })
 
@@ -77,6 +78,40 @@ test('Register client e-mail exist', async ({ client, assert }) => {
   const user = await User.findBy('email', 'newUser@gmail.com')
 
   assert.equal(user.toJSON().email, 'newUser@gmail.com')
+
+  Mail.restore()
+})
+
+test('Confirmation Mail', async ({ client, assert }) => {
+  Mail.fake()
+
+  const user = await client.post('/v1/auth/client/register')
+    .send({
+      username: 'newUser',
+      email: 'confirmationMail@gmail.com',
+      password: 'asdfasdf',
+      password_confirmation: 'asdfasdf',
+      phone: '12312312312',
+      birth: '2018-09-10 18:00:00'
+    })
+    .end()
+
+  const data = user.body.data
+
+  const confirmationMail = await ConfirmationMail.findByOrFail('user_id', data.id)
+
+  const token = confirmationMail.$originalAttributes.token
+  // simulo o usuário clicando no link enviado para seu e-mail
+  const confirmationResponse = await client.get('/v1/auth/confirmation_mail?token=' + token).end()
+
+  confirmationResponse.assertStatus(200)
+  confirmationResponse.assertJSONSubset({
+    message: 'Sua conta foi ativada com sucesso!'
+  })
+
+  // // tem que ter deletado a confirmação depois de ativar a conta
+  // const _confirmationMail = await ConfirmationMail.findBy('token', token)
+  // assert.equal(_confirmationMail, null)
 
   Mail.restore()
 })
