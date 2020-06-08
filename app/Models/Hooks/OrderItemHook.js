b77a9321696a908aa36d9fe1929362ea92482b3f
luanbrandao <luanbrandao4@gmail.com>
2020-06-08 00:32:29 -0300

    feat(orderitemhook): cálculo automático do subtotal do item
    Solves issue#16-feat-cadastrar-novo-pedidos

---
+++ app/Models/Hooks/OrderItemHook.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const OrderItemHook = (exports = module.exports = {})
+const Product = use('App/Models/Product')
+OrderItemHook.updateSubtotal = async modelInstance => {
+  const product = await Product.find(modelInstance.product_id)
+  if (product.type === 'amount') {
+    modelInstance.subtotal = modelInstance.quantity * product.price
+  } else if (product.type === 'weight') {
+    modelInstance.subtotal = (modelInstance.weight / product.weight) * product.price
+  }
+}
